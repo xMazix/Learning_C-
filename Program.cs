f18286dfae8321cc78c84026b559f45d1059e67f
xMazix <72320500+xMazix@users.noreply.github.com>
2020-10-03 23:14:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+    }
+}
