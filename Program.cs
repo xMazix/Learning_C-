4fd8805828a29e3b8e0469535a83f097b7a1c9af
xMazix <72320500+xMazix@users.noreply.github.com>
2020-10-04 00:04:35 +0200

    Add files via upload
    Program asks for a name and property. Then it write name and property.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Cviko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj, jak se jmenuješ ?");
+            string name = Console.ReadLine();
+            Console.WriteLine("A jaký tedy jsi, " + name + " ?");
+            string property = Console.ReadLine();
+            Console.WriteLine(name + " je " + property);
+            Console.ReadLine();
+        }
+    }
+}
